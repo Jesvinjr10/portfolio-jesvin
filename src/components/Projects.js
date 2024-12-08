8fa30d520b8b5a504caf3ef3e63234fb80041410
Jesvinjr10 <jesvinljose10@gmail.com>
2024-12-08 23:04:00 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,39 +1,57 @@
 import React from "react";
-import logo from "../assets/backhome.jpg"; // Correct path to the image
+import logo6 from "../assets/id5.png" // Correct path to the image
+import logo1 from "../assets/sxc.png"
+import logo2 from "../assets/id1.png"
+import logo3 from "../assets/id2.png"
+import logo4 from "../assets/id3.png"
+import logo5 from "../assets/id4.png"
+
+
 import "./project-section.css";
 
 const Projects = () => {
   const projects = [
     {
-      title: "USERDATA",
-      description:
-        "This project demonstrates fetching and displaying user data dynamically.",
-      image: logo,
-      link: "#",
-    },
-    {
-      title: "API USING AJAX",
+      title: "SXC ALUMNI MANAGNMENT SYSTEM ",
       description:
         "Fetching data from an API using AJAX for seamless web experience.",
-      image: logo,
+      image: logo1,
       link: "#",
     },
     {
       title: "STUDENT ID CARD",
       description: "A project for creating digital student ID cards.",
-      image: logo,
+      image: logo2,
       link: "#",
     },
     {
       title: "UI/UX DESIGN PROJECT",
       description: "An innovative UI/UX design project.",
-      image: logo,
+      image: logo3,
+      link: "#",
+    },
+    {
+      title: "UI/UX DESIGN PROJECT",
+      description: "An innovative UI/UX design project.",
+      image: logo4,
+      link: "#",
+    },
+    {
+      title: "UI/UX DESIGN PROJECT",
+      description: "An innovative UI/UX design project.",
+      image: logo5,
+      link: "#",
+    },
+    {
+      title: "UI/UX DESIGN PROJECT",
+      description: "An innovative UI/UX design project.",
+      image: logo6,
       link: "#",
     },
   ];
 
   return (
-    <div className="container-fluid py-5" style={{ backgroundColor: "#40534C" }}>
+    <div className="container-fluid py-5" id="projects" style={{ backgroundColor: "#40534C" }}>
       <h2 className="text-center text-light mb-4">My Projects</h2>
       <div className="row">
         {projects.map((project, index) => (
